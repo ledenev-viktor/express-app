748a883451996425b467bd1102f405f8cfd612ae
ledenev-viktor <ledenev4252@gmail.com>
2023-08-16 23:52:16 +0300

    обработка ошибок

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import { Server } from "http";
 import { LoggerService } from "./logger/logger.service";
 import { UserController } from "./users/users.controller";
+import { ExeptionFilter } from "./errors/exeption.filter";
 
 export class App {
   app: Express;
@@ -9,20 +10,31 @@ export class App {
   port: number;
   logger: LoggerService;
   userController: UserController;
+  exeptionFilter: ExeptionFilter;
 
-  constructor(logger: LoggerService, userController: UserController) {
+  constructor(
+    logger: LoggerService,
+    userController: UserController,
+    exeptionFilter: ExeptionFilter
+  ) {
     this.app = express();
     this.port = 8000;
     this.logger = logger;
     this.userController = userController;
+    this.exeptionFilter = exeptionFilter;
   }
 
   useRoutes() {
     this.app.use("/users", this.userController.router);
   }
 
+  useExeptionFilter() {
+    this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
+  }
+
   public async init() {
     this.useRoutes();
+    this.useExeptionFilter();
     this.server = this.app.listen(this.port);
     this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
   }
